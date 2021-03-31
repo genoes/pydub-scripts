33791f507935df8984f8fec9f29a23a0f0e0250a
Geno Sanchez <39529787+genoes@users.noreply.github.com>
2021-03-31 16:50:11 -0700

    Update audio-segment.py

--- audio-segment/audio-segment.py
+++ audio-segment/audio-segment.py
@@ -21,13 +21,20 @@ for i, chunk in enumerate(chunks, start = 1):
     chunk_name = './output/ihc_udelaire_DATE_{:03d}.mp3'.format(i)
     print('exporting', os.path.basename(chunk_name))
     chunk.export(chunk_name, format='mp3')
+
 print('\n'
 '''
-                     /|
-       =  =  =      / |
-  ____| || || |____/  | -_-_-_-_-_-_
-|)----| || || |____   |   FINISHED!
-  ((  | || || |  ))\  | _-_-_-_-_-_-
-   \\_|_||_||_|_//  \ |
-    \___________/    \|
+     ;
+     ;;
+     ;';.
+     ;  ;;
+     ;   ;;
+     ;    ;;
+     ;    ;;
+     ;   ;'
+     ;  '
+,;;;,;
+;;;;;;
+`;;;;'    Finished!
+
 ''''\n')
